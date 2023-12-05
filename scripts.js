90962246853fae27c44c6f2360812ddc74a44338
Umutcan Çetin <66206623+NeuralG@users.noreply.github.com>
2023-12-05 20:36:04 +0300

    Imported an answer array

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
+import { allowedAnswers } from "./words.js"
+import { allowedGuesses } from "./words.js"
+
 const input_list = document.querySelectorAll("input")
 input_list[0].focus()
 
-const answerList = ["FEYZA","HALIL","ONURA","UMUDO"]//GET A REAL DICTIONARY ALSO CODE THAT U CAN ONLY ENTER VALID LETTERS IN DICT
+const answerList = allowedAnswers
 const correctAnswer = answerList[Math.floor(Math.random()*answerList.length)]
 
 const popUpWinEl = document.getElementById("win-popup-out")
@@ -11,6 +14,7 @@ const answerEl = document.getElementById("answer")
 const attemptCountEl = document.getElementById("attempt-count")
 
 let letterList = []
+
 
 for (let i=0;i<30;i++){
 
@@ -32,52 +36,57 @@ for (let i=0;i<30;i++){
             input_list[i+1].focus()
         }else{
             if (content && event.key === "Enter"){ //CHECKS THE ANSWER
-                let myAnswer = `${input_list[i-4].value}${input_list[i-3].value}${input_list[i-2].value}${input_list[i-1].value}${input_list[i].value}`
-                for(let a=0;a<5;a++){
-                    if (myAnswer == correctAnswer){
-                        console.log("Tebrikler Kazandınız")
-                        for(let a=0; a<5; a++){
-                            input_list[i-4+a].style.backgroundColor = "green"
-                        }
-                        popUpWinEl.style.display = "flex"
-                        attemptCountEl.innerText = `You knew the word in ${attemptFinder(i)} attempt!!`
-                        break
-                    }
-                    else{
-                        letterList.push(input_list[i-4])
-                        letterList.push(input_list[i-3])
-                        letterList.push(input_list[i-2])
-                        letterList.push(input_list[i-1])
-                        letterList.push(input_list[i])
-                        for(let a=0;a<5;a++){
-                            if(correctAnswer[a] === myAnswer[a]){
+                if(allowedGuesses.includes(`${input_list[i-4].value}${input_list[i-3].value}${input_list[i-2].value}${input_list[i-1].value}${input_list[i].value}`)){
+                    let myAnswer = `${input_list[i-4].value}${input_list[i-3].value}${input_list[i-2].value}${input_list[i-1].value}${input_list[i].value}`
+                    for(let a=0;a<5;a++){
+                        if (myAnswer == correctAnswer){
+                            console.log("Tebrikler Kazandınız")
+                            for(let a=0; a<5; a++){
                                 input_list[i-4+a].style.backgroundColor = "green"
-                            }else{
-                                let isYellow = false
-                                for (let b = 0;b<letterList.length;b++){
-                                    if (myAnswer[a] == correctAnswer[b]){
-                                        isYellow = true
-                                    }
-                                }
-                                if (isYellow){
-                                    input_list[i-4+a].style.backgroundColor = "yellow"
+                            }
+                            popUpWinEl.style.display = "flex"
+                            attemptCountEl.innerText = `You knew the word in ${attemptFinder(i)} attempt!!`
+                            break
+                        }
+                        else{
+                            letterList.push(input_list[i-4])
+                            letterList.push(input_list[i-3])
+                            letterList.push(input_list[i-2])
+                            letterList.push(input_list[i-1])
+                            letterList.push(input_list[i])
+                            for(let a=0;a<5;a++){
+                                if(correctAnswer[a] === myAnswer[a]){
+                                    input_list[i-4+a].style.backgroundColor = "green"
                                 }else{
-                                    input_list[i-4+a].style.backgroundColor = "red"
+                                    let isYellow = false
+                                    for (let b = 0;b<letterList.length;b++){
+                                        if (myAnswer[a] == correctAnswer[b]){
+                                            isYellow = true
+                                        }
+                                    }
+                                    if (isYellow){
+                                        input_list[i-4+a].style.backgroundColor = "yellow"
+                                    }else{
+                                        input_list[i-4+a].style.backgroundColor = "red"
+                                    }
+                                    
+                                }
+                                if(i != 29){
+                                    input_list[i+1].focus()
+                                }else{
+                                    console.log("kaybettiniz")
+                                    popUpLoseEl.style.display = "flex"
+                                    answerEl.innerText = `The answer was: ${correctAnswer}`
+                                    break
                                 }
                                 
                             }
-                            if(i != 29){
-                                input_list[i+1].focus()
-                            }else{
-                                console.log("kaybettiniz")
-                                popUpLoseEl.style.display = "flex"
-                                answerEl.innerText = `The answer was: ${correctAnswer}`
-                                break
-                            }
                             
                         }
-                        
                     }
+                }
+                else{
+                    console.log("Your input isn't a proper english word")
                 }
             }
 
